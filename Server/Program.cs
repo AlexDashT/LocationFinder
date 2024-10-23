fa6298cc98124e8abdad2667c3ebcdecc32f22fc
Ali Dashti <ali.dashti@sportsbook-sw.com>
2024-10-23 12:12:45 +0200

    Upgarde to .Net 8

--- Server/Program.cs
+++ Server/Program.cs
@@ -34,7 +34,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseCors("Open");
 app.UseHttpsRedirection();
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
